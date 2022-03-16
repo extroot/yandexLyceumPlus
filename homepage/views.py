c9daf3110b553718c96782ce6adb67518e0a29c5
Pavel Pakhmutov <info@extroot.ru>
2022-03-16 23:12:53 +0300

    Add template render

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('<h1>Главная</h1>')
+    return render(request, 'home/index.html')
