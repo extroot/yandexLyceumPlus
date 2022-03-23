dd4f105dc0510607b937d870472b48116f75a032
Pavel Pakhmutov <info@extroot.ru>
2022-03-24 00:16:58 +0300

    Admin & migrations

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,2 +1,14 @@
 from django.contrib import admin
+from catalog.models import Item
+from catalog.models import Category, Tag
 
+admin.site.register(Category)
+admin.site.register(Tag)
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_published', 'text')
+    list_display_links = ('name', 'text')
+    list_editable = ('is_published',)
+    filter_horizontal = ('tags',)
