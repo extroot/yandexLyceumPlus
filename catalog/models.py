f834258cc847e9c05c3b1cfc5b70052f94376f3c
Pavel Pakhmutov <info@extroot.ru>
2022-03-23 23:46:23 +0300

    Add Item model

--- catalog/models.py
+++ catalog/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
+from core.models import Published
 
-class Item(models.Model):
-    name = models.CharField(max_length=255)
-    text = models.TextField(blank=True, null=True)
+
+class Item(Published):
+    name = models.CharField(max_length=150)
+    text = models.TextField(
+        verbose_name='Текст',
+        help_text='Минимум два слова. Обязательно должно содержаться слово превосходно или роскошно'
+    )
+    # Todo: Validator
     is_published = models.BooleanField(default=False)
 
     class Meta:
-        verbose_name = 'Элемент'
-        verbose_name_plural = 'Элементы'
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
