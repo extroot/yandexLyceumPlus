9d9fe2acffc4e511f22a7b4f456cfa94af6df4e3
Pavel Pakhmutov <info@extroot.ru>
2022-04-06 22:36:05 +0300

    Add prefetch_related

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,15 @@
+from django.db.models import Prefetch
 from django.http import Http404
 from django.shortcuts import render
 
 from catalog.models import Item
+from catalog.models import Tag
 
 
 def item_list(request):
-    all_items = Item.objects.filter(is_published=True)
+    all_items = Item.objects.filter(is_published=True).prefetch_related(
+        Prefetch('tags', queryset=Tag.objects.filter(is_published=True))
+    )
     context = {
         'items': all_items
     }
@@ -16,6 +20,7 @@ def item_detail(request, id_product):
     try:
         item = Item.objects.get(pk=id_product)
         if not item.is_published:
+            # Не уверен нужно ли это, но пусть будет.
             raise Http404()
     except Item.DoesNotExist:
         raise Http404()
--- homepage/views.py
+++ homepage/views.py
@@ -1,9 +1,11 @@
 import random
 
 from django.db.models import Max
+from django.db.models import Prefetch
 from django.shortcuts import render
 
 from catalog.models import Item
+from catalog.models import Tag
 
 
 def home(request):
@@ -11,9 +13,14 @@ def home(request):
 
     # Не самый быстрый способ получения всех опубликованных товаров,
     #  но один из самых простых.
+    # Прочитал, что .order_by('?') ненадёжен.
     ids = list(Item.objects.filter(is_published=True).values_list('id', flat=True))
     if len(ids) < 3:
-        context['items'] = Item.objects.filter(is_published=True)
+        context['items'] = Item.objects.filter(is_published=True).prefetch_related(
+            Prefetch('tags', queryset=Tag.objects.filter(is_published=True))
+        )
     else:
-        context['items'] = Item.objects.filter(pk__in=random.sample(ids, 3))
+        context['items'] = Item.objects.filter(pk__in=random.sample(ids, 3)).prefetch_related(
+            Prefetch('tags', queryset=Tag.objects.filter(is_published=True))
+        )
     return render(request, 'homepage/home.html', context=context)
