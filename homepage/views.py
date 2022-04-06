8a8cc229d17bbda27b27c73c2208827b35a64a87
Pavel Pakhmutov <info@extroot.ru>
2022-04-06 20:37:42 +0300

    Home page functionality

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,19 @@
+import random
+
+from django.db.models import Max
 from django.shortcuts import render
+
+from catalog.models import Item
 
 
 def home(request):
     context = {}
+
+    # Не самый быстрый способ получения всех опубликованных товаров,
+    #  но один из самых простых.
+    ids = list(Item.objects.filter(is_published=True).values_list('id', flat=True))
+    if len(ids) < 3:
+        context['items'] = Item.objects.filter(is_published=True)
+    else:
+        context['items'] = Item.objects.filter(pk__in=random.sample(ids, 3))
     return render(request, 'homepage/home.html', context=context)
