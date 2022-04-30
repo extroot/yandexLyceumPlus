3f1ed5b4cb4fc08c686de041ada812ebc3989dbc
Amir <g.amir2004@gmail.com>
2022-04-30 17:12:12 +0300

    add birthday context processor

--- yandexLyceum/settings.py
+++ yandexLyceum/settings.py
@@ -8,7 +8,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Add env
 load_dotenv(os.path.join(BASE_DIR, '.env'))
 
-
 # Sentry (Default off)
 # Only for better debug
 if os.environ.get('USE_SENTRY') == 'True':
@@ -23,12 +22,9 @@ if os.environ.get('USE_SENTRY') == 'True':
         send_default_pii=True
     )
 
-
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-
 DEBUG = os.environ.get('DEBUG', False) == 'True'
-
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -80,13 +76,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'users.context_processors.birthdays_today',
             ],
         },
     },
 ]
 
 WSGI_APPLICATION = 'yandexLyceum.wsgi.application'
-
 
 # Database
 
@@ -96,7 +92,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 
@@ -115,7 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 
 LANGUAGE_CODE = 'ru'
@@ -125,7 +119,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 
@@ -139,7 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/auth/login'
 LOGIN_REDIRECT_URL = '/auth/profile'
-
 
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_HOST_USER = 'yandex_lyceum@extroot.ru'
