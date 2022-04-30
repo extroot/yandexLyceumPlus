5844f02b3790f8f3f23ed0e55db72f5b2a4d106b
Pavel Pakhmutov <info@extroot.ru>
2022-04-30 22:35:59 +0300

    Fix validation

--- catalog/validators.py
+++ catalog/validators.py
@@ -1,11 +1,11 @@
 from django.forms import ValidationError
+from django.utils.html import strip_tags
 
 
 def text_validation(text: str):
     must_words = {'превосходно', 'роскошно'}
     if len(text.split()) < 2:
         raise ValidationError('Минимально необходимо 2 слова')
-
-    lower_text = set(text.lower().split())
+    lower_text = set(strip_tags(text).lower().split())
     if len(lower_text) == len(lower_text - must_words):
         raise ValidationError(f'Необходимо использовать одно из слов: {must_words}')
